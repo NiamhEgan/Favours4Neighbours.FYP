8ee95a454733b8cac0a0d8e035059b7c4e727987
NiamhEgan <77689705+NiamhEgan@users.noreply.github.com>
2021-03-01 21:19:28 +0000

    Databse data moves to .env file for security

--- Favours4Neighbours.CodeIgniter.php/app/Config/Database.php
+++ Favours4Neighbours.CodeIgniter.php/app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => 'root',
+		'username' => '',
 		'password' => '',
-		'database' => 'favours4neighbours',
+		'database' => '',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
