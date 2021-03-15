be3b4e39b85d026cbcaababd0fea22cec56f8f5c
NiamhEgan <77689705+NiamhEgan@users.noreply.github.com>
2021-03-15 21:28:48 +0000

    path

--- Favours4Neighbours.CodeIgniter.php/app/Views/LoginView.php
+++ Favours4Neighbours.CodeIgniter.php/app/Views/LoginView.php
@@ -1,6 +1,6 @@
 <?php $usernameValue = isset($username) ? 'value="' . $username . '"' : ""; ?>
 <main class="form-signin" style="width: 400px;">
-	<form action="/login/manage/" method="post">
+	<form method="post">
 		<h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
 		<label class="visually-hidden" for="inputEmail">Username</label>
@@ -10,7 +10,7 @@
 		<input class="form-control" id="Password" name="Password" placeholder="Password" required type="password">
 
 		<div class="checkbox mb-3"><label><input type="checkbox" value="remember-me"> Remember me</label></div>
-		<button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+		<button class="w-100 btn btn-lg btn-primary" name="LoginButton" type="submit">Sign in</button>
 
 		<?php if (isset($error)) { ?><p class="error"><?php echo $error ?></p><?php } ?>
 	</form>
