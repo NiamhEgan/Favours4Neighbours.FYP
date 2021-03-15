ac09a3e6bbb55264468e229d335802a0110e132b
NiamhEgan <77689705+NiamhEgan@users.noreply.github.com>
2021-03-15 20:28:22 +0000

    bootstrap

--- Favours4Neighbours.CodeIgniter.php/app/Views/masterpage.php
+++ Favours4Neighbours.CodeIgniter.php/app/Views/masterpage.php
@@ -5,61 +5,80 @@
     <title><?php echo $title; ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 </head>
 
 <body>
     <header>
-        <nav>
-            <div class="navigation">
-                <ul>
-                    <li class="menu-toggle">
-                        <button onclick="toggleMenu();">&#9776;</button>
-                    </li>
-                    <li class="menu-item hidden"><a href="#">Home</a></li>
-                    <li class="menu-item hidden"><a href="#">Edit Profile</a></li>
-                    <li class="menu-item hidden"><a href="#">Jobs</a></li>
-                    <li class="menu-item hidden"><a href="#">Logout</a></li>
-                </ul>
-            </div>
-        </nav>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"> Profile</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Jobs
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">View Jobs</a>
+          <a class="dropdown-item" href="#">Create Job</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">View Applications</a>
+        </div>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        FAQ's
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">FAQ's</a>
+          <a class="dropdown-item" href="#">COVID-19</a>
+          <a class="dropdown-item" href="#">Safety Declaration</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">View Applications</a>
+        </div>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#"> Logout</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+        
     </header>
     <?php echo $mainContent; ?>
     <aside>
-        <div class="col-sm-4">
+        <div class="col-sm">
             <h3>Profile </h3>
             <p>Profile Image </p>
 
             <img src="">
             <p> Profile Description <br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
         </div>
-        <div class="col-sm-4">
-            <h3>Quick Access</h3>
-            <ul>
-                </li>
-                <li class="Create-Jobs-CTA"><a href="#">Create Job </a></li>
-                <li class="Create-Jobs-CTA"><a href="#">My Jobs </a></li>
-                <li class="Create-Jobs-CTA"><a href="#">Search Jobs</a></li>
-            </ul>
-        </div>
+       
     </aside>
 
     <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <h3>Useful Information</h3>
-                    <ul>
-                        <li class="menu-item hidden"><a href="#">About Us </a></li>
-                        <li class="menu-item hidden"><a href="#">COVID-19 </a></li>
-                        <li class="menu-item hidden"><a href="#">FAQ's</a></li>
-                        <li class="menu-item hidden"><a href="#">Safety Declareation</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        
     </footer>
 </body>
 
