23fdfb79dfe3bcbd7323790aeff02f3dbe5effb8
NiamhEgan <77689705+NiamhEgan@users.noreply.github.com>
2021-04-07 17:01:00 +0100

    bat luancher

---
+++ Favours4Neighbours.CodeIgniter.php/run.bat
@@ -0,0 +1,11 @@
+REM mysql -u {user} -p favours4neighbours < Favours4Neighbours.mysql.sql` to create a database
+
+REM to install required codeigniter packages
+
+echo composer i 
+
+REM to launch
+php spark serve
+
+echo Now about to end...
+pause
