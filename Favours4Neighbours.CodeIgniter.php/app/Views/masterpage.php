414ee3a42fc21a2e9fa0efb852140606781eabbd
NiamhEgan <77689705+NiamhEgan@users.noreply.github.com>
2021-04-07 14:49:32 +0100

    view clean up

--- Favours4Neighbours.CodeIgniter.php/app/Views/masterpage.php
+++ Favours4Neighbours.CodeIgniter.php/app/Views/masterpage.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
 
-  <?php echo $mainContent; ?>
+<body>
+  <?php echo view('templates/header'); ?>
+  <?php if (isset($navTemplate)) : ?>
+    <?php echo view('templates/' . $navTemplate); ?>
+  <?php else : ?>
+    <?php echo view('templates/nav-public.php'); ?>
+  <?php endif ?>
+  <main><?php echo $mainContent; ?></main>
+  
+  <?php echo view('templates/footer'); ?>
+</body>
+
+</html>
