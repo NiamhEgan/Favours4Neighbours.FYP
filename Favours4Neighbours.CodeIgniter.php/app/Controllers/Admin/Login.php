0ae8481757ddec920def2e254be68ef4871ea9a1
NiamhEgan <77689705+NiamhEgan@users.noreply.github.com>
2021-04-21 19:27:41 +0100

    Applications viewManager

--- Favours4Neighbours.CodeIgniter.php/app/Controllers/Admin/Login.php
+++ Favours4Neighbours.CodeIgniter.php/app/Controllers/Admin/Login.php
@@ -97,30 +97,5 @@ class Login extends BaseController
 		echo view('MasterPage', $data);
 	}
 
-	public function demo()
-	{
-		$demoUserId = 100; //	Admin
-		$user  = $this->UserRepository->find($demoUserId);
 
-		var_dump($user);
-		if ($user != null) {
-			//$this->websitemanager->createUserSession($user->Id, $user->Username);
-			//redirect("/home");
-			$data = [
-				'mainContent' => "Login Success",
-				'title' => "Favours 4 Neighbours",
-			];
-			echo view('MasterPage', $data);
-		} else {
-			$loginData = [
-				"error" => "Incorrect login details entered",
-				"username" => "Admin Demo"
-			];
-			$data = [
-				'mainContent' => view("LoginView", $loginData),
-				'title' => "Favours 4 Neighbours",
-			];
-			echo view('MasterPage', $data);
-		}
-	}
 }
