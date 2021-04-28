bd02b593dcf1cc8b8e20847e4531aee33ac54692
NiamhEgan <77689705+NiamhEgan@users.noreply.github.com>
2021-04-28 16:59:07 +0100

    applications

--- app/Views/MyApplicationsView.php
+++ app/Views/MyApplicationsView.php
@@ -23,4 +23,59 @@
 			<?php endforeach ?>
 		<?php endif ?>
 	</tbody>
+
+	
+</table>
+<table class="table table-striped">
+	<caption class="caption-top">My Accepted Applications</caption>
+	<thead>
+		<tr>
+			<th scope="col">Date</th>
+			<th scope="col">Job Details</th>
+			<th scope="col">Status</th>
+			<td>&nbsp;</td>
+		</tr>
+	</thead>
+	<tbody>
+		<?php if ($jobApplications != null) : ?>
+			<?php foreach ($jobApplications as $jobApplication) : ?>
+				<tr>
+					<td><?= $jobApplication->created_at; ?></td>
+					<td><a href="/client/applications/myapplications/<?= $jobApplication->JobId; ?>"><?= $jobApplication->JobDetails; ?></a></td>
+					<td><?= $jobApplication->Status; ?></td>
+					<td><a href="/client/applications/reject/<?= $jobApplication->Id; ?>">Withdraw</a></td>
+					<td><a href="/client/applications/complete/<?= $jobApplication->Id; ?>">Complete</a></td>
+					
+				</tr>
+
+
+			<?php endforeach ?>
+		<?php endif ?>
+	</tbody>
+</table>
+<table class="table table-striped">
+	<caption class="caption-top">My Rejected Applications</caption>
+	<thead>
+		<tr>
+			<th scope="col">Date</th>
+			<th scope="col">Job Details</th>
+			<th scope="col">Status</th>
+			<td>&nbsp;</td>
+		</tr>
+	</thead>
+	<tbody>
+		<?php if ($jobApplications != null) : ?>
+			<?php foreach ($jobApplications as $jobApplication) : ?>
+				<tr>
+					<td><?= $jobApplication->created_at; ?></td>
+					<td><a href="/client/applications/myapplications/<?= $jobApplication->JobId; ?>"><?= $jobApplication->JobDetails; ?></a></td>
+					<td><?= $jobApplication->Status; ?></td>
+					<td><a href="/client/applications/reject/<?= $jobApplication->Id; ?>">Withdraw</a></td>
+					
+				</tr>
+
+
+			<?php endforeach ?>
+		<?php endif ?>
+	</tbody>
 </table>
